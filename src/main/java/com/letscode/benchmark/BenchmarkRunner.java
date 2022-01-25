28a84e2630226caf5545c57f088f297286106917
Leon Watanabe <leon.watanabe@gmail.com>
2022-01-25 19:11:07 -0300

    refac: refactoring main for better comprehension

--- src/main/java/com/letscode/benchmark/BenchmarkRunner.java
+++ src/main/java/com/letscode/benchmark/BenchmarkRunner.java
@@ -1,43 +1,53 @@
 package com.letscode.benchmark;
 
 import com.letscode.sort.QuickSort;
-
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class BenchmarkRunner {
 
-    public static void main(String[] args) throws Exception {
-//        Options options = new OptionsBuilder()
-//                .include(SortBenchmark.class.getSimpleName())
-//                .build();
-//
-//        new Runner(options).run();
-
-//        org.openjdk.jmh.Main.main(args);
-
+  public static void main(String[] args) throws Exception {
+    opcaoExecucaoUsandoBenchmarkMain(args);
+//        opcaoExecucaoUsandoRunnerComOpcoes();
 //        benchmarkManual();
 
+  }
+
+  private static void opcaoExecucaoUsandoBenchmarkMain(String[] args) throws IOException {
+    org.openjdk.jmh.Main.main(args);
+  }
+
+  private static void opcaoExecucaoUsandoRunnerComOpcoes() throws RunnerException {
+    Options options = new OptionsBuilder()
+        .include(SortBenchmark.class.getSimpleName())
+        .build();
+
+    new Runner(options).run();
+  }
+
+  private static void benchmarkManual() {
+    int[] array = BenchmarkUtils.generateIntArray(100000);
+    QuickSort quickSort = new QuickSort();
+
+    //warmup para executar o jit
+    for (int i = 0; i < 100; i++) {
+      quickSort.execute(array);
     }
 
-    private static void benchmarkManual() {
-        int[] array = BenchmarkUtils.generateIntArray(100000);
-        QuickSort quickSort = new QuickSort();
+    //benchmark
+    LocalDateTime start = LocalDateTime.now();
+    System.out.println(start);
 
-        //warmup para executar o jit
-        for (int i = 0; i < 100; i++) {
-            quickSort.execute(array);
-        }
+    quickSort.execute(array);
 
-        //benchmark
-        LocalDateTime start = LocalDateTime.now();
-        System.out.println(start);
+    LocalDateTime end = LocalDateTime.now();
+    System.out.println(end);
 
-        quickSort.execute(array);
-
-        LocalDateTime end = LocalDateTime.now();
-        System.out.println(end);
-
-        System.out.println("Tempo: " + start.until(end, ChronoUnit.MICROS));
-    }
+    System.out.println("Tempo: " + start.until(end, ChronoUnit.MICROS));
+  }
 }
